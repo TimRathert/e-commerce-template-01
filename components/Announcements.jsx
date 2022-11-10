77e00445ea0c239dcc6cf1178d65de7bc28d2985
TimRathert <rathertt@gmail.com>
2022-11-10 12:20:57 -0500

    working on navbar

--- components/Announcements.jsx
+++ components/Announcements.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Announcements() {
   return (
     <div className=' w-full bg-[#3e5445] text-white py-2 flex flex-col md:flex-row justify-center items-center'>
-       <p className='tracking-wider text-xs'>BUY ONLINE + PICK UP IN STORE</p> 
-       <p className='text-xs'> - select “store pick up” at checkout + pick up same day!</p>
+       <p className='tracking-wider text-[10px] font-semibold'>BUY ONLINE + PICK UP IN STORE</p> 
+       <p className='text-[10px]'> - select “store pick up” at checkout + pick up same day!</p>
     </div>
   )
 }
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Menu } from '@headlessui/react'
 
 function NavBar() {
+    const navToggle = () => {
+
+    }
   return (
-    <div>NavBar</div>
+    <div className='flex justify-between p-2'>
+        <div className='md:hidden'>Drop small</div>
+        <div className='sm:max-md:hidden'>nav medium up</div>
+        <div className='text-3xl font-light'>Beans Shop</div>
+        <div className=''>search / cart</div>
+    </div>
   )
 }
 
