ed40d82515d5df37f7791fc8a21cfb29f6ad6428
TimRathert <rathertt@gmail.com>
2022-11-10 14:53:53 -0500

    working on carousel

--- components/MainSlideShow.jsx
+++ components/MainSlideShow.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Transition } from '@headlessui/react'
+
 
 function MainSlideShow(prop) {
+    let currentSlide = 1
+    const slideShow = () => {
+        let i
+
+    }
   return (
-    <div>
-        <img src='https://via.placeholder.com/1500/' className='w-screen max-h-[90vh]'/>
+    <div className='flex snap-x snap-mandatory overflow-hidden'>
+        {/* <img src='https://via.placeholder.com/1500/' className='w-screen max-h-[90vh]'/> */}
+        <div className='snap-start min-w-full h-[300px] max-h-[90vh] flex items-center justify-center' id='slide-1'>Slide 1</div>
+        <div className='snap-start min-w-full h-[300px] max-h-[90vh] flex items-center justify-center' id='slide-2'>Slide 2</div>
+        <div className='snap-start min-w-full h-[300px] max-h-[90vh] flex items-center justify-center' id='slide-3'>Slide 3</div>
     </div>
   )
 }
