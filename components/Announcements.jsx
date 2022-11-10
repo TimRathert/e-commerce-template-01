09f9bd7320e11100211e4a39b2944a6c9ced34c5
TimRathert <rathertt@gmail.com>
2022-11-10 11:32:07 -0500

    copying shopdressup.com initial commit

---
+++ components/Announcements.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Announcements() {
+  return (
+    <div className=' w-full bg-[#3e5445] text-white py-2 flex flex-col md:flex-row justify-center items-center'>
+       <p className='tracking-wider text-xs'>BUY ONLINE + PICK UP IN STORE</p> 
+       <p className='text-xs'> - select “store pick up” at checkout + pick up same day!</p>
+    </div>
+  )
+}
+
+export default Announcements
